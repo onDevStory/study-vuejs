09005118537447bfb4d04b60a55031d1d0659a8d
jhsung-PC\jhsung <jhsung@hubilon.co.kr>
2017-03-29 17:39:04 +0900

    Components (Using components - Local Registration)

--- componentEx.js
+++ componentEx.js
@@ -3,7 +3,15 @@ Vue.component('my-component', {
 	template: '<div>A custom component!</div>'
 })
 
+// register a local component
+var Child = {
+	template: '<div>A local component!</div>'
+}
+
 // create a root instance
 new Vue({
-	el: '#component-example'
+	el: '#component-example',
+	components: {
+		'child-component': Child
+	}
 })
