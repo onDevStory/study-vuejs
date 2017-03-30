ac35ce65efdf63f07cdb535137cb4f5667cb541a
jhsung-PC\jhsung <jhsung@hubilon.co.kr>
2017-03-30 15:56:54 +0900

    Components (Custom Events - Using v-on with Custom Events)

--- componentEx.js
+++ componentEx.js
@@ -49,6 +49,21 @@ Vue.component('exam01', {
 	}
 })
 
+Vue.component('button-counter', {
+	template: '<button v-on:click="increment">{{ counter }}</button>',
+	data: function() {
+		return {
+			counter: 0
+		}
+	},
+	methods: {
+		increment: function() {
+			this.counter += 1
+			this.$emit('increment')
+		}
+	},
+})
+
 // create a root instance
 var vm = new Vue({
 	el: '#component-example',
@@ -62,6 +77,15 @@ var vm = new Vue({
 		obj: {
 			lang: 'VueJS',
 			version: '2.2.0'
+		},
+		total: 0
+	},
+	methods: {
+		incrementTotal: function() {
+			this.total += 1
+		},
+		nativeEvent: function() {
+			alert('nativeEvent')
 		}
 	}
 })
