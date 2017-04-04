be9a25082c7fe369854920ac3e802864ec69e569
jhsung-PC\jhsung <jhsung@hubilon.co.kr>
2017-04-04 14:17:35 +0900

    props in the vue instance (this.props 로 사용하면 된다는 건가?)

--- vue-cli/src/components/UserDetail.vue
+++ vue-cli/src/components/UserDetail.vue
@@ -2,13 +2,18 @@
 	<div class="component">
 		<h3>You may view the User Details here</h3>
 		<p>Many Details</p>
-		<p>User Name: {{ userName }}</p>
+		<p>User Name: {{ userName }} | reverse: {{ switchName() }}</p>
 	</div>
 </template>
 
 <script>
 export default {
-	props: ['userName']
+	props: ['userName'],
+	methods: {
+		switchName() {
+			return this.userName.split('').reverse().join('');
+		}
+	}
 }
 </script>
 
